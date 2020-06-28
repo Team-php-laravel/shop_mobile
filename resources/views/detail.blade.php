83e3b616e86c21ce5296c9cbe141c824b440a7ae
xuanhanh.dev <xuanhanh.dev@fgct.net>
2020-06-28 23:36:26 +0700

    ok

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -242,7 +242,7 @@
                             </div>
                         </div>
                         @if($sp->mo_ta != '')
-                            <div class="product-description d-flex">
+                            <div class="product-description" style="display: flex">
                                 <div class="sidebar-box black" style="height: auto">
                                     {!! $sp->mo_ta !!}
                                     <div class="clear"></div>
